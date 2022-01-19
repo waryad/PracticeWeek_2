907b8a9fc378db13fe7b33c55e136b830e2e9a98
waryad <waryad@sheridancollege.ca>
2022-01-19 14:08:14 -0500

    version 1

---
+++ src/main/java/ReverseWord.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author deep
+ */
+import java.util.Scanner;
+
+
+public class ReverseWord {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // 
+      Scanner in =new  Scanner(System.in);
+     System.out.print("Enter the name");
+     
+    String name = in.nextLine();
+    name = strint ot char
+            
+            
+      
+    }
+    
+}
